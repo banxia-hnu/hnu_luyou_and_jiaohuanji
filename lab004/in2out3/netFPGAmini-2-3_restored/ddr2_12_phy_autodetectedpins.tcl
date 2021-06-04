d39d6993313e7257f7cc120d3a078aa7a4bc3a93
banxia-hnu <2350700380@qq.com>
2021-06-04 15:42:54 +0800

    lab004
    lab004

---
+++ lab004/in2out3/netFPGAmini-2-3_restored/ddr2_12_phy_autodetectedpins.tcl
@@ -0,0 +1,25 @@
+ddr_pin ck_p ddr2_ck pins
+ddr_pin dqsgroup {{ddr2_dqs[0]} {{ddr2_dm[0]}} {{ddr2_dq[0]} {ddr2_dq[1]} {ddr2_dq[2]} {ddr2_dq[3]} {ddr2_dq[4]} {ddr2_dq[5]} {ddr2_dq[6]} {ddr2_dq[7]}} {ddr2_dqs_n[0]}} pins
+ddr_pin dqsgroup {{ddr2_dqs[1]} {{ddr2_dm[1]}} {{ddr2_dq[10]} {ddr2_dq[11]} {ddr2_dq[12]} {ddr2_dq[13]} {ddr2_dq[14]} {ddr2_dq[15]} {ddr2_dq[8]} {ddr2_dq[9]}} {ddr2_dqs_n[1]}} pins
+ddr_pin ck_n ddr2_ck_n pins
+ddr_pin addrcmd {ddr2_addr[0]} pins
+ddr_pin addrcmd {ddr2_addr[10]} pins
+ddr_pin addrcmd {ddr2_addr[11]} pins
+ddr_pin addrcmd {ddr2_addr[12]} pins
+ddr_pin addrcmd {ddr2_addr[1]} pins
+ddr_pin addrcmd {ddr2_addr[2]} pins
+ddr_pin addrcmd {ddr2_addr[3]} pins
+ddr_pin addrcmd {ddr2_addr[4]} pins
+ddr_pin addrcmd {ddr2_addr[5]} pins
+ddr_pin addrcmd {ddr2_addr[6]} pins
+ddr_pin addrcmd {ddr2_addr[7]} pins
+ddr_pin addrcmd {ddr2_addr[8]} pins
+ddr_pin addrcmd {ddr2_addr[9]} pins
+ddr_pin addrcmd {ddr2_bank_addr[0]} pins
+ddr_pin addrcmd {ddr2_bank_addr[1]} pins
+ddr_pin addrcmd ddr2_cas_n pins
+ddr_pin addrcmd ddr2_ras_n pins
+ddr_pin addrcmd ddr2_we_n pins
+ddr_pin addrcmd ddr2_cke pins
+ddr_pin addrcmd ddr2_odt pins
+ddr_pin addrcmd ddr2_cs_n pins
